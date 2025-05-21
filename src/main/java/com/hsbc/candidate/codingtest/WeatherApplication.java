6c2088a72d3cd932e090ca1657197522ce4cf41f
josepharuja <joseph.aruja@mastek.com>
2025-05-21 21:30:42 +0100

    TEST-0000: refactoring in progress

--- src/main/java/com/hsbc/candidate/codingtest/WeatherApplication.java
+++ src/main/java/com/hsbc/candidate/codingtest/WeatherApplication.java
@@ -6,6 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WeatherApplication {
 
+    /**
+     * Private constructor to prevent instantiation of this utility class.
+     */
+    private WeatherApplication() {
+        // Private constructor to hide the implicit public one
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(WeatherApplication.class, args);
     }
