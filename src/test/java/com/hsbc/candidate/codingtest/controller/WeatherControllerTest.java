d6ace581be87ff1b73c3039f4cf2e4e5bebcdce1
josepharuja <joseph.aruja@mastek.com>
2025-05-21 20:51:40 +0100

    TEST-0000: refactoring in progress

--- src/test/java/com/hsbc/candidate/codingtest/controller/WeatherControllerTest.java
+++ src/test/java/com/hsbc/candidate/codingtest/controller/WeatherControllerTest.java
@@ -1,7 +1,6 @@
 package com.hsbc.candidate.codingtest.controller;
 
 import com.hsbc.candidate.codingtest.exception.GlobalExceptionHandler;
-import com.hsbc.candidate.codingtest.exception.WeatherServiceException;
 import com.hsbc.candidate.codingtest.model.City;
 import com.hsbc.candidate.codingtest.model.WeatherResponse;
 import com.hsbc.candidate.codingtest.service.WeatherService;
@@ -9,13 +8,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.http.HttpStatus;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import java.util.Arrays;
-import java.util.List;
 
 import static org.mockito.Mockito.when;
 
@@ -26,14 +23,12 @@ public class WeatherControllerTest {
     @Mock
     private WeatherService weatherService;
 
-    private WeatherController weatherController;
 
     @BeforeEach
     public void setup() {
         MockitoAnnotations.openMocks(this);
-        weatherController = new WeatherController(weatherService);
         webTestClient = WebTestClient
-                .bindToController(weatherController)
+                .bindToController( new WeatherController(weatherService))
                 .controllerAdvice(new GlobalExceptionHandler())
                 .build();
     }
@@ -70,27 +65,6 @@ public class WeatherControllerTest {
                 .isEqualTo(response);
     }
 
-    @Test
-    public void testGetAllWeatherData_ServiceException() {
-        WeatherServiceException exception = new WeatherServiceException(
-                WeatherServiceException.SERVICE_UNAVAILABLE,
-                "External weather service is unavailable."
-        );
-        when(weatherService.fetchWeatherData()).thenReturn(Mono.error(exception));
-
-        StepVerifier.create(weatherService.fetchWeatherData())
-                .expectErrorMatches(err -> err instanceof WeatherServiceException &&
-                        ((WeatherServiceException) err).getErrorCode().equals(WeatherServiceException.SERVICE_UNAVAILABLE) &&
-                        err.getMessage().equals("External weather service is unavailable."))
-                .verify();
-
-        webTestClient.get()
-                .uri("/api/weather")
-                .exchange()
-                .expectStatus().isEqualTo(HttpStatus.SERVICE_UNAVAILABLE)
-                .expectBody()
-                .jsonPath("$.message").isEqualTo("External weather service is unavailable.");
-    }
 
     @Test
     public void testCountCitiesStartingWith() {
@@ -108,25 +82,4 @@ public class WeatherControllerTest {
                 .jsonPath("$.count").isEqualTo(1);
     }
 
-    @Test
-    public void testGetCitiesStartingWith_InvalidData() {
-        WeatherServiceException exception = new WeatherServiceException(
-                WeatherServiceException.INVALID_DATA,
-                "Weather response contains no cities data"
-        );
-        when(weatherService.getCitiesStartingWith("Z")).thenReturn(Mono.error(exception));
-
-        StepVerifier.create(weatherService.getCitiesStartingWith("Z"))
-                .expectErrorMatches(err -> err instanceof WeatherServiceException &&
-                        ((WeatherServiceException) err).getErrorCode().equals(WeatherServiceException.INVALID_DATA) &&
-                        err.getMessage().equals("Weather response contains no cities data"))
-                .verify();
-
-        webTestClient.get()
-                .uri("/api/weather/cities?letter=Z")
-                .exchange()
-                .expectStatus().isEqualTo(HttpStatus.BAD_REQUEST)
-                .expectBody()
-                .jsonPath("$.message").isEqualTo("Weather response contains no cities data");
-    }
 }
