77d710fc4ebfccc8e78c01e9102ea6ac0d913d53
josepharuja <joseph.aruja@mastek.com>
2025-05-22 12:39:21 +0100

    TEST-0000: code improvements

--- src/main/java/com/hsbc/candidate/codingtest/model/Rain.java
+++ src/main/java/com/hsbc/candidate/codingtest/model/Rain.java
@@ -3,8 +3,17 @@ package com.hsbc.candidate.codingtest.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
+/**
+ * Represents rainfall information in the weather data.
+ * This class contains data about precipitation volume over different time periods.
+ */
 @Data
+@SuppressWarnings("PMD.ShortClassName") // Rain is an appropriate name for this domain model
 public class Rain {
+    /**
+     * The rainfall volume for the last 3 hours, in millimeters.
+     * This field is mapped from the JSON property "3h" in the API response.
+     */
     @JsonProperty("3h")
     private double threeHour;
 }
